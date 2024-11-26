9474eb56a5aa39fde6ea21acc636dce7228d7799
Ahdaa <mosil.biz@gmail.com>
2024-11-27 02:40:14 +0800

    update view and readme.md

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database_demo/news_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -28,6 +29,19 @@ class _HomePageState extends State<HomePage> {
             const Text(
               "Flutter x Firebase",
               style: TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // 進入 news_page.dart
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const NewsPage()),
+                );
+              },
+              child: const Text("進入"),
+            ),
+            const SizedBox(
+              height: 8,
             ),
             Wrap(
               children: [
---
+++ lib/news.dart
@@ -0,0 +1,9 @@
+class News {
+  String title;
+  String context;
+
+  News({
+    required this.title,
+    required this.context,
+  });
+}
